7d784c50296b58cd977e96513260179105895e69
aAbdelnaby88 <ahmed.abdelnapy88@gmail.com>
2020-04-05 16:07:13 +0200

    edit subnets

--- subnets.tf
+++ subnets.tf
@@ -4,7 +4,7 @@ resource "aws_subnet" "firstPublic" {
 
     cidr_block = "10.0.1.0/28"
     availability_zone ="us-east-2c"
-    
+    map_public_ip_on_launch =true
    
     tags = {
         Name = "firstPublic"
@@ -17,7 +17,8 @@ resource "aws_subnet" "secondPublic" {
 
     cidr_block = "10.0.2.0/28"
     availability_zone ="us-east-2a"
-  
+    map_public_ip_on_launch = true
+    
     tags = {
         Name = "secondPublic"
     }
