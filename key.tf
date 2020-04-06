bbcebdbe7fce87780ea4c683840b25fa20d65d6a
aAbdelnaby88 <ahmed.abdelnapy88@gmail.com>
2020-04-06 22:50:29 +0200

    update key generator

--- key.tf
+++ key.tf
@@ -11,15 +11,20 @@ resource "aws_secretsmanager_secret" "newPrivate" {
 resource "aws_secretsmanager_secret_version" "example" {
   secret_id     = aws_secretsmanager_secret.newPrivate.id
   secret_string = tls_private_key.privateKey.private_key_pem
-
-    provisioner "local-exec" {
-    command = "aws secretsmanager get-secret-value --secret-id newKey --profile test --region us-east-2 > privateKey.pem"
-    interpreter = ["/bin/bash", "-c"]
-  }
 }
 
 resource "aws_key_pair" "deployer" {
   key_name   = "deployer-key"
   public_key = tls_private_key.privateKey.public_key_openssh
+}
+
+output "private_key" {
+  value = tls_private_key.privateKey.private_key_pem
+  description = "The TLS genreated private key value"
+}
+
+resource "local_file" "privateKey" {
+    content     = tls_private_key.privateKey.private_key_pem
+    filename = "./pKey.pem"
 }
 
